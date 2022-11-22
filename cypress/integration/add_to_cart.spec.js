9af610ded1ac25229d24e61a25cfb62c275738ca
Hanson Lee <leehanson@alumni.ubc.ca>
2022-11-22 23:39:23 +0000

    Test that cart count goes up when adding item to cart

---
+++ cypress/integration/add_to_cart.spec.js
@@ -0,0 +1,25 @@
+describe('home-page', () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit('/');
+  })
+
+  it("Click first product", () => {
+    cy.get(".nav-item")
+    .contains("My Cart")
+    .should("contain", "My Cart (0)")
+
+    cy.get(".products article")
+    .first()
+    .get(".btn")
+    .contains("Add")
+    .click({force: true})
+
+    cy.get(".nav-item")
+    .contains("My Cart")
+    .should("contain", "My Cart (1)")
+  });
+})
